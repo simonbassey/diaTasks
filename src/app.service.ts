9a22e639fc79bea961a705888b8060bdd1c518f8
simonbassey <simon.dev.bassey@gmail.com>
2020-11-22 04:11:18 +0100

    Changed entry api endpoint to serve REST styled ReadMe

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,15 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  launchWelcomeNote(): any {
+    return {
+      version: `1.0.0`,
+      summary: 'Welcome to home for APIs for oragnising your daily life tasks .. one activity/event at a time',
+      authors: [
+        `simonbassey[simon.dev.bassey@gmail.com]`
+      ]
+
+    }
+  }
 }
